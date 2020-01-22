5cce1b273666cfa9d186d543ada04ce5f3ae5b0f
jcalvarezj <jcalvarezj@gmail.com>
2020-01-22 14:02:09 -0500

    Added oscillating colors to particles

--- Swarm.hpp
+++ Swarm.hpp
@@ -13,7 +13,7 @@ namespace ParticleSimulation {
 
 class Swarm {
 public:
-	const static int N_PARTICLES = 500;
+	const static int N_PARTICLES = 5000;
 private:
 	Particle * m_particles;
 public:
--- main.cpp
+++ main.cpp
@@ -32,25 +32,20 @@ int main(int argc, char ** args) {
 
 		const Particle * const particles = swarm.getParticles();
 
+		int ellapsed = SDL_GetTicks();
+
+		Uint8 red = (1 + sin(ellapsed * 0.001)) * 128;
+		Uint8 green = (1 + sin(ellapsed * 0.002)) * 128;
+		Uint8 blue = (1 + sin(ellapsed * 0.003)) * 128;
+
 		for(int i = 0; i < Swarm::N_PARTICLES; i++) {
 			Particle particle = particles[i];
 
 			int x = (particle.m_x + 1) * Screen::S_WIDTH / 2;
 			int y = (particle.m_y + 1) * Screen::S_HEIGHT / 2;
 
-			screen.setPixel(x, y, 255, 255, 255);
+			screen.setPixel(x, y, red, green, blue);
 		}
-
-		/*
-		int ellapsed = SDL_GetTicks();
-
-		Uint8 green = (1 + sin(ellapsed * 0.001)) * 128;
-
-		std::cout << "Now: " << (int)green << std::endl;
-
-		for (int i = 0; i < Screen::S_HEIGHT; i++)
-			for (int j = 0; j < Screen::S_WIDTH; j++)
-				screen.setPixel(j, i, 0, green, 0);*/
 
 		screen.update();
 
