5df7864b35033da77a06d1aca9e1b50b81349c8c
jcalvarezj <jcalvarezj@gmail.com>
2020-01-22 13:35:32 -0500

    Corrected Particle position generation

--- Particle.cpp
+++ Particle.cpp
@@ -10,8 +10,8 @@
 namespace ParticleSimulation {
 
 Particle::Particle() {
-	m_x = 2.0 * rand() / RAND_MAX;
-	m_y = 2.0 * rand() / RAND_MAX;
+	m_x = (2.0 * rand() / RAND_MAX) - 1;
+	m_y = (2.0 * rand() / RAND_MAX) - 1;
 }
 
 } // namespace ParticleSimulation
